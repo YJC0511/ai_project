d7bf411de2831c14d1570c72fa8633f56c68f21c
YJC0511 <yejunch0511@gmail.com>
2025-11-03 13:14:21 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title('시그너스왕국의 마왕을 물리쳐주세요!')
 name=st.text_input('용사님의 이름을 알려주세요!')
-st.selectbox('원하시는 서번트를 선택해주세요:',['아처','세이버']
+st.selectbox('원하시는 서번트를 선택해주세요:',['아처','세이버'])
 if st.button('인사말 생성'):
   st.info(name+'용사님! 안녕하세요!')
   st.warning('우리 왕국을 도와주세요!')
