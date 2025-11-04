9029555556cdf5d89319896bce7f561853a734cd
YJC0511 <yejunch0511@gmail.com>
2025-11-04 14:42:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title('시그너스왕국의 마왕을 물리쳐주세요!')
 name=st.text_input('용사님의 이름을 알려주세요!')
-menu=st.selectbox('원하시는 성유물을 선택해주세요:', ['아발론','토오사카 린의
+menu=st.selectbox('원하시는 성유물을 선택해주세요:' , ['아발론','토오사카 린의
 10년 치 마력이 담긴 보석'])
 if st.button('인사말 생성'):
   st.info(name+'용사님! 안녕하세요!')
